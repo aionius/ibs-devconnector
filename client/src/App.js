a24882c7183b1337fcd10fbcc2587fc8d8950af8
aionius <aionius@gmail.com>
2019-11-22 16:10:59 -0500

    implemented post/message functionality to the application

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,8 @@ import Profile from "./components/profile/Profile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
@@ -94,6 +96,12 @@ class App extends React.Component {
                            path="/add-education"
                            component={AddEducation}
                         />
+                     </Switch>
+                     <Switch>
+                        <PrivateRoute exact path="/feed" component={Posts} />
+                     </Switch>
+                     <Switch>
+                        <PrivateRoute exact path="/post/:id" component={Post} />
                      </Switch>
                      <Route exact path="/not-found" component={NotFound} />
                   </div>
